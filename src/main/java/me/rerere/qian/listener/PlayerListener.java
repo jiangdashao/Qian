c88a01ffc67c57c844bf4146f357c5ae53c04b1d
jiangdashao <qq1609403959@outlook.com>
2020-09-30 17:52:56 +0800

    action test

--- src/main/java/me/rerere/qian/listener/PlayerListener.java
+++ src/main/java/me/rerere/qian/listener/PlayerListener.java
@@ -5,11 +5,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         Qian.getInstance().getBalanceManager().asyncLoad(player);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        
     }
 }
